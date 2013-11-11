6cc668b6b99b034a2c964de367523017ac74adc5
Heeso <palttuk40@nate.com>
2013-11-11 21:50:03 +0900

    Initial Commit

---
+++ Study/StoryboardTest/StoryboardTestTests/StoryboardTestTests.m
@@ -0,0 +1,34 @@
+//
+//  StoryboardTestTests.m
+//  StoryboardTestTests
+//
+//  Created by Heeso KIM on 2013. 11. 11..
+//  Copyright (c) 2013년 Heeso KIM. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StoryboardTestTests : XCTestCase
+
+@end
+
+@implementation StoryboardTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
